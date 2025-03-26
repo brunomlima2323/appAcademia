8f6cecfadb777a926a08bda4e358ac7974aeb6df
Bruno Lima <bruno.lima@lynxspa.com>
2025-03-26 13:35:45 -0300

    add logs configuracao banco

--- src/main/java/br/com/bml/appAcademia/AppAcademiaApplication.java
+++ src/main/java/br/com/bml/appAcademia/AppAcademiaApplication.java
@@ -7,6 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppAcademiaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("=================================");
+		System.out.println("Iniciando aplicação");
+		System.out.println("DATABASE_URL: jdbc:postgresql://" +  System.getenv("PGHOST") + ":" +  System.getenv("PGPORT") + "/" + System.getenv("PGDATABASE"));
+		System.out.println("PGHOST: " + System.getenv("PGHOST"));
+		System.out.println("PGPORT: " + System.getenv("PGPORT"));
+		System.out.println("PGDATABASE: " + System.getenv("PGDATABASE"));
+		System.out.println("PGUSER: " + System.getenv("PGUSER"));
+		System.out.println("PGPASSWORD: " + System.getenv("PGPASSWORD"));
+		System.out.println("=================================");
+		
 		SpringApplication.run(AppAcademiaApplication.class, args);
 	}
 
