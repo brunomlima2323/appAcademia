b96c89ca5de64b2b54f31e69f1b025996bda1aba
Bruno Lima <bruno.lima@lynxspa.com>
2025-12-23 19:19:40 -0300

    config para receber requisições safari

--- src/main/java/com/bml/appAcademia/infra/security/SecurityConfigurations.java
+++ src/main/java/com/bml/appAcademia/infra/security/SecurityConfigurations.java
@@ -1,10 +1,12 @@
 package com.bml.appAcademia.infra.security;
 
+import com.bml.appAcademia.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,10 +43,28 @@ public class SecurityConfigurations {
             .build();
     }
 
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+//        return configuration.getAuthenticationManager();
+//    }
+
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
-        return configuration.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(
+            HttpSecurity http,
+            PasswordEncoder passwordEncoder,
+            UsuarioRepository usuarioRepository // ou seu UserDetailsService
+    ) throws Exception {
+
+        AuthenticationManagerBuilder authBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authBuilder
+                .userDetailsService(login -> usuarioRepository.findByLogin(login))
+                .passwordEncoder(passwordEncoder); // ESSA LINHA RESOLVE TUDO
+
+        return authBuilder.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
